90f36a8bdffaf04915754b88cb4814afdef441e6
Luigi A. A. Sansonetti <luigi@sansonetti.ch>
2019-11-21 15:40:31 +0100

    Added the file names

--- src/clouds.cpp
+++ src/clouds.cpp
@@ -7,7 +7,40 @@
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
- 
+
+static const double min = -1.5;
+static const double max = 1.5;
+static const double range = max-min;
+static const double numColors = 100;
+
+static const std::string files[] = {
+    "../../data/cli/cli_10.vti_scaled.vti",
+    "../../data/cli/cli_20.vti_scaled.vti",
+    "../../data/cli/cli_30.vti_scaled.vti",
+    "../../data/clw/clw_10.vti_scaled.vti",
+    "../../data/clw/clw_20.vti_scaled.vti",
+    "../../data/clw/clw_30.vti_scaled.vti",
+    "../../data/pres/pres_10.vti_scaled.vti",
+    "../../data/pres/pres_20.vti_scaled.vti",
+    "../../data/pres/pres_30.vti_scaled.vti",
+    "../../data/qr/qr_10.vti_scaled.vti",
+    "../../data/qr/qr_20.vti_scaled.vti",
+    "../../data/qr/qr_30.vti_scaled.vti",
+    "../../data/ua/ua_10.vti_scaled.vti",
+    "../../data/ua/ua_20.vti_scaled.vti",
+    "../../data/ua/ua_30.vti_scaled.vti",
+    "../../data/va/va_10.vti_scaled.vti",
+    "../../data/va/va_20.vti_scaled.vti",
+    "../../data/va/va_30.vti_scaled.vti",
+    "../../data/wa/wa_10.vti_scaled.vti",
+    "../../data/wa/wa_20.vti_scaled.vti",
+    "../../data/wa/wa_30.vti_scaled.vti",
+    
+};
+
+void readImageData(vtkSmartPointer<vtkXMLImageDataReader> &reader, std::string fileName);
+void getColorCorrespondingTovalue(double val, double &r, double &g, double &b);
+
 int main(int argc, char* argv[]) {
     // Verify number of input arguments
     if (argc != 2) {
@@ -15,15 +48,15 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
     
-    // Get filename from cli arguments
-    std::string inputFilename = argv[1];
-    
-    // Read the file
+    // Reading the file
+    std::string fileName = argv[1];
     vtkSmartPointer<vtkXMLImageDataReader> reader = vtkSmartPointer<vtkXMLImageDataReader>::New();
-    reader->SetFileName(inputFilename.c_str());
-    reader->Update();
+    readImageData(reader, fileName);
     
-    // Visualize
+    
+    
+    
+    // Visualising
     vtkSmartPointer<vtkDataSetMapper> mapper = vtkSmartPointer<vtkDataSetMapper>::New();
     mapper->SetInputConnection(reader->GetOutputPort());
     
@@ -53,3 +86,34 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
+void readImageData(vtkSmartPointer<vtkXMLImageDataReader> &reader, std::string fileName) {
+    reader->SetFileName(fileName.c_str());
+    reader->Update();
+}
+
+void getColorCorrespondingTovalue(double val, double &r, double &g, double &b) {
+    static const int numColorNodes = 9;
+    double color[numColorNodes][3] = {
+        {0.6980, 0.0941, 0.1686}, // Red
+        {0.8392, 0.3765, 0.3020},
+        {0.9569, 0.6471, 0.5098},
+        {0.9922, 0.8588, 0.7804},
+        {0.9686, 0.9686, 0.9686}, // White
+        {0.8196, 0.8980, 0.9412},
+        {0.5725, 0.7725, 0.8706},
+        {0.2627, 0.5765, 0.7647},
+        {0.1294, 0.4000, 0.6745}  // Blue
+    };
+    
+    for (int i = 0; i < (numColorNodes - 1); i++) {
+        double currFloor = min + ((double)i / (numColorNodes - 1)) * range;
+        double currCeil = min + ((double)(i + 1) / (numColorNodes - 1)) * range;
+        
+        if ((val >= currFloor) && (val <= currCeil)) {
+            double currFraction = (val - currFloor) / (currCeil - currFloor);
+            r = color[i][0] * (1.0 - currFraction) + color[i + 1][0] * currFraction;
+            g = color[i][1] * (1.0 - currFraction) + color[i + 1][1] * currFraction;
+            b = color[i][2] * (1.0 - currFraction) + color[i + 1][2] * currFraction;
+        }
+    }
+}
