3710fb43528cb13b825fd68e9f6261fe5cf86a6a
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-11-29 14:54:33 +0100

    attempt to convert to polydata

--- src/clouds.cpp
+++ src/clouds.cpp
@@ -1,15 +1,14 @@
-#include <vtkVersion.h>
-#include <vtkXMLImageDataReader.h>
-#include <vtkImageData.h>
 #include <vtkSmartPointer.h>
+#include <vtkProperty.h>
+#include <vtkDataSetMapper.h>
+#include <vtkImageActor.h>
+#include <vtkImageViewer2.h>
+#include <vtkXMLImageDataReader.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkInteractorStyleImage.h>
 #include <vtkRenderer.h>
-#include <vtkImageMapper.h>
-#include <vtkImageResliceMapper.h>
-#include <vtkImageSlice.h>
-
+#include <vtkPolyDataMapper.h>
+#include <vtkImageDataGeometryFilter.h>
 
 static const std::string files[] = {
     "../../cloud_data/cli/cli_10.vti_scaled.vti",
@@ -40,15 +39,63 @@ static void CreateColorImage(vtkImageData*);
 
 int main(int, char *[]) {
     // Read file
+    std::cout << "reading data...";
     vtkSmartPointer<vtkXMLImageDataReader> reader = vtkSmartPointer<vtkXMLImageDataReader>::New();
     reader->SetFileName(files[0].c_str());
     reader->Update();
+    std::cout << " [done]" << std::endl;
 
+    /*
+    // Visualize
+    vtkSmartPointer<vtkDataSetMapper> mapper =
+        vtkSmartPointer<vtkDataSetMapper>::New();
+    mapper->SetInputConnection(reader->GetOutputPort());
+    */
+
+    // Covert image to polydata
+    std::cout << "converting to polydata";
+    vtkSmartPointer<vtkImageDataGeometryFilter> imageDataGeometryFilter = vtkSmartPointer<vtkImageDataGeometryFilter>::New();
+    imageDataGeometryFilter->SetInputConnection(reader->GetOutputPort());
+    imageDataGeometryFilter->Update();
+    std::cout << " [done]" << std::endl;
+
+    // Create mapper and actor
+    std::cout << "creating poly data mapper";
+    vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(imageDataGeometryFilter->GetOutputPort());
+    std::cout << " [done]" << std::endl;
+
+    vtkSmartPointer<vtkActor> actor =
+        vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+
+    vtkSmartPointer<vtkRenderer> renderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor(actor);
+    renderer->SetBackground(1,1,1);
+
+    vtkSmartPointer<vtkRenderWindow> renderWindow =
+        vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetSize(1500, 1000);
+
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+        vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+    renderWindowInteractor->Initialize();
+
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
+
+/*
     vtkSmartPointer<vtkImageData> colorImage = vtkSmartPointer<vtkImageData>::New();
     // CreateColorImage(colorImage);
     // colorImage->Print(std::cerr);
     colorImage = reader->GetOutput();
     colorImage->AllocateScalars(VTK_FLOAT, 3);
+    colorImage->Print(std::cerr);
 
     vtkSmartPointer<vtkImageResliceMapper> imageResliceMapper = vtkSmartPointer<vtkImageResliceMapper>::New();
     imageResliceMapper->SetInputData(colorImage);
@@ -83,18 +130,5 @@ int main(int, char *[]) {
     renderWindowInteractor->Start();
 
     return EXIT_SUCCESS;
+*/
 }
-
-void CreateColorImage(vtkImageData* image) {
-    image->SetDimensions(10, 10, 1);
-    image->AllocateScalars(VTK_UNSIGNED_CHAR,3);
-    for(unsigned int x = 0; x < 10; x++) {
-        for(unsigned int y = 0; y < 10; y++) {
-            unsigned char* pixel = static_cast<unsigned char*>(image->GetScalarPointer(x,y,0));
-            pixel[0] = 255;
-            pixel[1] = 0;
-            pixel[2] = 255;
-        }
-    }
-}
-
