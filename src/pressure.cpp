fb5a6cdbcdd87fd4bfe79bd5df7e2978ab9b018b
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-12-09 15:22:13 +0100

    created wind file and read vti files for components and stuff

--- src/pressure.cpp
+++ src/pressure.cpp
@@ -25,13 +25,6 @@
 #include <vtkProperty.h>
 #include <vtkImageProperty.h>
 
-// Vector Field
-#include <vtkArrowSource.h>
-#include <vtkCellArray.h>
-#include <vtkGlyph2D.h>
-#include <vtkPolyData.h>
-#include <vtkPoints.h>
-
 // Slicing
 #include <vtkImageSliceMapper.h>
 #include <vtkInteractorStyleTrackballCamera.h>
@@ -78,46 +71,39 @@ public:
         if (state == 0) {
             // disable time _30
             renderer->RemoveActor(imageSlices[2]);
-            renderer->RemoveActor(vectorActors[2]);
             renderer->RemoveActor2D(texts[2]);
             sliderWidgets[2]->EnabledOff();
-
+            
             // enable time _10
             renderer->AddActor(imageSlices[0]);
-            renderer->AddActor(vectorActors[0]);
             renderer->AddActor2D(texts[0]);
             sliderWidgets[0]->EnabledOn();
         } else if (state == 1) {
             // disable time _10
             renderer->RemoveActor(imageSlices[0]);
-            renderer->RemoveActor(vectorActors[0]);
             renderer->RemoveActor2D(texts[0]);
             sliderWidgets[0]->EnabledOff();
-
+            
             // enable time _20
             renderer->AddActor(imageSlices[1]);
-            renderer->AddActor(vectorActors[1]);
             renderer->AddActor2D(texts[1]);
             sliderWidgets[1]->EnabledOn();
         } else {
             // disable time _20
             renderer->RemoveActor(imageSlices[1]);
-            renderer->RemoveActor(vectorActors[1]);
             renderer->RemoveActor2D(texts[1]);
             sliderWidgets[1]->EnabledOff();
-
+            
             // enable time _30
             renderer->AddActor(imageSlices[2]);
-            renderer->AddActor(vectorActors[2]);
             renderer->AddActor2D(texts[2]);
             sliderWidgets[2]->EnabledOn();
         }
-
+        
     }
-    vtkButtonCallback() : sliderWidgets(std::vector<vtkSmartPointer<vtkSliderWidget>>(0)), imageSlices(std::vector<vtkSmartPointer<vtkImageSlice>>(0)), vectorActors(std::vector<vtkSmartPointer<vtkActor>>(0)), renderer(0), actor(0), renderWindow(0), renderWindowInteractor(0), styleTrackball(0), texts(std::vector<vtkSmartPointer<vtkTextActor>>(0)){}
+    vtkButtonCallback() : sliderWidgets(std::vector<vtkSmartPointer<vtkSliderWidget>>(0)), imageSlices(std::vector<vtkSmartPointer<vtkImageSlice>>(0)), renderer(0), actor(0), renderWindow(0), renderWindowInteractor(0), styleTrackball(0), texts(std::vector<vtkSmartPointer<vtkTextActor>>(0)){}
     std::vector<vtkSmartPointer<vtkSliderWidget>> sliderWidgets;
     std::vector<vtkSmartPointer<vtkImageSlice>> imageSlices;
-    std::vector<vtkSmartPointer<vtkActor>> vectorActors;
     vtkSmartPointer<vtkRenderer> renderer;
     vtkSmartPointer<vtkActor> actor;
     vtkSmartPointer<vtkRenderWindow> renderWindow;
@@ -127,17 +113,63 @@ public:
 };
 
 static const std::string files[] = {
+    "../../cloud_data/cli/cli_10.vti_scaled.vti",
+    "../../cloud_data/cli/cli_20.vti_scaled.vti",
+    "../../cloud_data/cli/cli_30.vti_scaled.vti",
+    "../../cloud_data/clw/clw_10.vti_scaled.vti",
+    "../../cloud_data/clw/clw_20.vti_scaled.vti",
+    "../../cloud_data/clw/clw_30.vti_scaled.vti",
     "../../cloud_data/pres/pres_10.vti_scaled.vti",
     "../../cloud_data/pres/pres_20.vti_scaled.vti",
-    "../../cloud_data/pres/pres_30.vti_scaled.vti"
+    "../../cloud_data/pres/pres_30.vti_scaled.vti",
+    "../../cloud_data/qr/qr_10.vti_scaled.vti",
+    "../../cloud_data/qr/qr_20.vti_scaled.vti",
+    "../../cloud_data/qr/qr_30.vti_scaled.vti",
+    "../../cloud_data/ua/ua_10.vti_scaled.vti",
+    "../../cloud_data/ua/ua_20.vti_scaled.vti",
+    "../../cloud_data/ua/ua_30.vti_scaled.vti",
+    "../../cloud_data/va/va_10.vti_scaled.vti",
+    "../../cloud_data/va/va_20.vti_scaled.vti",
+    "../../cloud_data/va/va_30.vti_scaled.vti",
+    "../../cloud_data/wa/wa_10.vti_scaled.vti",
+    "../../cloud_data/wa/wa_20.vti_scaled.vti",
+    "../../cloud_data/wa/wa_30.vti_scaled.vti",
+    
 };
 
-void CreateImage(vtkSmartPointer<vtkImageData> image, unsigned char* color1, unsigned char* color2, unsigned char* color3) {
+
+void getColorCorrespondingToValue(double min, double max, double range, double numColors,
+                                  double val, double &r, double &g, double &b) {
+    static const int numColorNodes = 9;
+	double color[numColorNodes][3] = {
+        {0.0314, 0.3176, 0.6118}, // Blue
+        {0.1922, 0.5098, 0.7412},
+        {0.4196, 0.6824, 0.8392},
+        {0.6196, 0.7922, 0.8824},
+        {0.7765, 0.8588, 0.9373},
+        {0.9373, 0.9529, 1.0000} // White
+	};
+
+	for (int i = 0; i < (numColorNodes - 1); i++) {
+		double currFloor = min + ((double)i / (numColorNodes - 1)) * range;
+		double currCeil = min + ((double)(i + 1) / (numColorNodes - 1)) * range;
+
+		if ((val >= currFloor) && (val <= currCeil)) {
+			double currFraction = (val - currFloor) / (currCeil - currFloor);
+			r = color[i][0] * (1.0 - currFraction) + color[i + 1][0] * currFraction;
+			g = color[i][1] * (1.0 - currFraction) + color[i + 1][1] * currFraction;
+			b = color[i][2] * (1.0 - currFraction) + color[i + 1][2] * currFraction;
+		}
+	}
+}
+
+void CreateImage(vtkSmartPointer<vtkImageData> image, unsigned char* color1, unsigned char* color2, unsigned char* color3)
+{
     // Specify the size of the image data
     image->SetDimensions(15, 15, 1);
     image->AllocateScalars(VTK_UNSIGNED_CHAR, 3);
     int* dims = image->GetDimensions();
-
+    
     // Fill the image with
     for (int y = 0; y < dims[1]; y++) {
         for (int x = 0; x < dims[0]; x++) {
@@ -160,21 +192,21 @@ void CreateImage(vtkSmartPointer<vtkImageData> image, unsigned char* color1, uns
 }
 
 int main(int, char *[]) {
-    cout << "This program will display the cloud pressure data" << endl << std::flush;
+    cout << "This program will display the cloud ice data" << endl << std::flush;
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Creating the renderer and window interactor
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
     renderWindow->SetSize(1500, 1000);
-
+    
     vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
     renderer->SetBackground(1, 1, 1);
     renderWindow->AddRenderer(renderer);
-
+    
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
     renderWindowInteractor->SetRenderWindow(renderWindow);
     renderer->SetBackground(1, 1, 1);
-
+    
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Creating the button to select what time slot to display
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -189,25 +221,25 @@ int main(int, char *[]) {
     CreateImage(image1, green, gray, blue);
     CreateImage(image2, gray, blue, green);
     CreateImage(image2, blue, green, gray);
-
+    
     // Create the widget and its representation
     vtkSmartPointer<vtkTexturedButtonRepresentation2D> buttonRepresentation = vtkSmartPointer<vtkTexturedButtonRepresentation2D>::New();
     buttonRepresentation->SetNumberOfStates(3);
     buttonRepresentation->SetButtonTexture(0, image1);
     buttonRepresentation->SetButtonTexture(1, image2);
     buttonRepresentation->SetButtonTexture(2, image3);
-
+    
     vtkSmartPointer<vtkButtonWidget> buttonWidget = vtkSmartPointer<vtkButtonWidget>::New();
     buttonWidget->SetInteractor(renderWindowInteractor);
     buttonWidget->SetRepresentation(buttonRepresentation);
-
-
+    
+    
     // Place the widget. Must be done after a render so that the viewport is defined.
     // Here the widget placement is in normalized display coordinates
     vtkSmartPointer<vtkCoordinate> upperLeft = vtkSmartPointer<vtkCoordinate>::New();
     upperLeft->SetCoordinateSystemToNormalizedDisplay();
     upperLeft->SetValue(0, 1.0);
-
+    
     double bds[6];
     double sz = 50.0;
     bds[0] = upperLeft->GetComputedDisplayValue(renderer)[0] - sz;
@@ -215,13 +247,13 @@ int main(int, char *[]) {
     bds[2] = upperLeft->GetComputedDisplayValue(renderer)[1] - sz;
     bds[3] = bds[2] + sz;
     bds[4] = bds[5] = 0.0;
-
+    
     // Scale to 1, default is .5
     buttonRepresentation->SetPlaceFactor(1);
     buttonRepresentation->PlaceWidget(bds);
     buttonWidget->On();
     cerr << " done" << endl;
-
+    
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Reading the files
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -232,72 +264,95 @@ int main(int, char *[]) {
         vtkSmartPointer<vtkXMLImageDataReader> reader = vtkSmartPointer<vtkXMLImageDataReader>::New();
         reader->SetFileName(file.c_str());
         reader->Update();
-
+        
         readers[i] = reader;
         cerr << " done" << endl;
     }
-
-
+    
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// Creating the images
+    /// Creating the images and lookup tables
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    cerr << "Creating the images...";
+    cerr << "Creating the images and lookup table...";
     std::vector<vtkSmartPointer<vtkImageData>> images(3);
-    for (int i = 0; i < 3; i++) {
-      // Create an image
-      vtkSmartPointer<vtkImageData> image = vtkSmartPointer<vtkImageData>::New();
-      image = readers[i]->GetOutput();
-      // A better way to do this is (should be tested for compilablilty and correctness):
-      // TODO: check if name is correct (via paraview what exactly we need to set as active)
-      // image->GetPointData()->SetActiveVectors(image->GetPointData()->GetScalars()->GetName().c_str());
-      image->GetPointData()->SetActiveVectors("pres");
-      images[i] = image;
+    std::vector<vtkSmartPointer<vtkLookupTable>> lookupTables(3);
+    for (int i = 0 ; i < 3 ; i++) {
+        vtkSmartPointer<vtkImageData> image = vtkSmartPointer<vtkImageData>::New();
+        image = readers[i]->GetOutput();
+        image->GetPointData()->SetActiveScalars("cli");
+        
+        if (DEBUG) {
+            cerr << endl;
+            image->Print(cerr);
+        }
+        images[i] = image;
+
+        // Get image values range
+        float valuesRange[2];
+        /// WARNING: Following only works for .cli files
+        vtkFloatArray::SafeDownCast(image->GetPointData()->GetAbstractArray("cli"))->GetValueRange(valuesRange);
+
+        double min = valuesRange[0];
+        double max = valuesRange[1];
+        if (DEBUG) {
+            cerr << "min = " << min << ", max = " << max << endl;
+        }
+        double range = max-min;
+        double numColors = 100;
+
+        // Create color lookup table
+        vtkSmartPointer<vtkLookupTable> lookupTable = vtkSmartPointer<vtkLookupTable>::New();
+        lookupTable->SetScaleToLinear();
+        lookupTable->SetNumberOfTableValues(numColors);
+        double r, g, b;
+        for (int i = 0; i < numColors; i++) {
+            double val = min + ((double) i / numColors) * range;
+            getColorCorrespondingToValue(min, max, range, numColors, val, r, g, b);
+            lookupTable->SetTableValue(i, r, g, b);
+        }
+        lookupTable->SetRange(min, max);
+        lookupTable->Build();
+        
+        if (DEBUG) {
+            cerr << endl << endl;
+            lookupTable->Print(cerr);
+        }
+        lookupTables[i] = lookupTable;
     }
     cerr << " done" << endl;
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// Creating glyph filters
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    cerr << "Creating the glyph filters...";
-    std::vector<vtkSmartPointer<vtkGlyph2D>> glyphFilters(3);
-    for (int i = 0; i < 3; i++) {
-      // Setup the arrows
-      vtkSmartPointer<vtkArrowSource> arrowSource = vtkSmartPointer<vtkArrowSource>::New();
-      arrowSource->Update();
-
-      vtkSmartPointer<vtkGlyph2D> glyphFilter = vtkSmartPointer<vtkGlyph2D>::New();
-      glyphFilter->SetSourceConnection(arrowSource->GetOutputPort());
-      glyphFilter->OrientOn();
-      glyphFilter->SetVectorModeToUseVector();
-      glyphFilter->SetInputData(images[i]);
-      glyphFilter->Update();
-
-      glyphFilters[i] = glyphFilter;
-    }
-
-
+    
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Slicing
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     cerr << "Slicing...";
     std::vector<vtkSmartPointer<vtkImageSliceMapper>> imageSliceMappers(3);
     std::vector<vtkSmartPointer<vtkImageSlice>> imageSlices(3);
-    std::vector<vtkSmartPointer<vtkPolyDataMapper>> vectorMappers(3);
     for (int i = 0 ; i < 3 ; i++) {
-      vtkSmartPointer<vtkImageSliceMapper> imageMapper = vtkSmartPointer<vtkImageSliceMapper>::New();
-      imageMapper->SetInputData(images[i]);
-      imageSliceMappers[i] = imageMapper;
-
-      vtkSmartPointer<vtkImageSlice> imageSlice = vtkSmartPointer<vtkImageSlice>::New();
-      imageSlice->SetMapper(imageMapper);
-      imageSlices[i] = imageSlice;
-
-      vtkSmartPointer<vtkPolyDataMapper> vectorMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-      vectorMapper->SetInputConnection(glyphFilters[i]->GetOutputPort());
-      vectorMappers[i] = vectorMapper;
+        vtkSmartPointer<vtkImageSliceMapper> imageSliceMapper = vtkSmartPointer<vtkImageSliceMapper>::New();
+        imageSliceMapper->SetInputData(images[i]);
+        imageSliceMapper->SetSliceNumber(74); // Starting with a nice slice
+        imageSliceMapper->Update();
+        
+        if (DEBUG) {
+            cerr << endl;
+            imageSliceMapper->Print(cerr);
+        }
+        imageSliceMappers[i] = imageSliceMapper;
+        
+        vtkSmartPointer<vtkImageSlice> imageSlice = vtkSmartPointer<vtkImageSlice>::New();
+        imageSlice->SetMapper(imageSliceMapper);
+        imageSlice->GetProperty()->SetLookupTable(lookupTables[i]);
+        imageSlice->GetProperty()->UseLookupTableScalarRangeOn();
+        imageSlice->Update();
+        
+        if (DEBUG) {
+            cerr << endl;
+            imageSlice->Print(cerr);
+        }
+        imageSlices[i] = imageSlice;
     }
     cerr << " done" << endl;
-
+    
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Creating the slice sliders
@@ -323,26 +378,26 @@ int main(int, char *[]) {
         sliderRep->GetPoint1Coordinate()->SetValue(40, 40);
         sliderRep->GetPoint2Coordinate()->SetCoordinateSystemToDisplay();
         sliderRep->GetPoint2Coordinate()->SetValue(190, 40);
-
+        
         sliderReps[i] = sliderRep;
-
+        
         vtkSmartPointer<vtkSliderWidget> sliderWidget = vtkSmartPointer<vtkSliderWidget>::New();
         sliderWidget->SetInteractor(renderWindowInteractor);
         sliderWidget->SetRepresentation(sliderRep);
         sliderWidget->SetAnimationModeToAnimate();
-
+        
         // create the callback
         vtkSmartPointer<vtkSliderCallback> callback = vtkSmartPointer<vtkSliderCallback>::New();
         callback->sliceMapper = imageSliceMappers[i];
         sliderWidget->AddObserver(vtkCommand::InteractionEvent, callback);
-
+        
         sliderWidgets[i] = sliderWidget;
     }
     sliderWidgets[0]->EnabledOn();
-   
+    
     cerr << " done" << endl;
-
-
+    
+    
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Setting up the text
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -359,25 +414,14 @@ int main(int, char *[]) {
         textActors[i]->GetTextProperty()->SetColor (1.0, 0.0, 0.0);
     }
     cerr << " done" << endl;
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// Setting up the vector actors
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    cerr << "Setting up the vector actors...";
-    std::vector<vtkSmartPointer<vtkActor>> vectorActors(3);
-    for (int i = 0; i < 3; i++) {
-      // Create actors
-      vtkSmartPointer<vtkActor> vectorActor = vtkSmartPointer<vtkActor>::New();
-      vectorActor->SetMapper(vectorMappers[i]);
-    }
-    cerr << " done" << endl;
-
+    
+    
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Setting up the renderers
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     cerr << "Setting up the renderers...";
+    renderer->AddViewProp(imageSlices[0]);
     renderer->AddActor(imageSlices[0]);
-    renderer->AddActor(vectorActors[0]);
     renderer->AddActor2D(textActors[0]);
     renderer->ResetCamera();
     renderer->SetBackground(1, 1, 1);
@@ -389,13 +433,13 @@ int main(int, char *[]) {
     vtkSmartPointer<vtkInteractorStyleTrackballCamera> style = vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
     renderWindowInteractor->SetInteractorStyle(style);
     cerr << " done" << endl;
-
+    
+    
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Enabling the time button
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     vtkSmartPointer<vtkButtonCallback> callbackButton = vtkSmartPointer<vtkButtonCallback>::New();
     callbackButton->imageSlices = imageSlices;
-    callbackButton->vectorActors = vectorActors;
     callbackButton->sliderWidgets = sliderWidgets;
     callbackButton->renderer = renderer;
     callbackButton->actor = renderer->GetActors()->GetLastActor();
@@ -403,7 +447,7 @@ int main(int, char *[]) {
     callbackButton->renderWindowInteractor = renderWindowInteractor;
     callbackButton->styleTrackball = style;
     callbackButton->texts = textActors;
-
+    
     buttonWidget->AddObserver(vtkCommand::StateChangedEvent, callbackButton);
 
     // Render and start interaction
