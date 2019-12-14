001eb422930be2ecc2e12e6d9fffc960fa754381
Luigi A. A. Sansonetti <luigi@sansonetti.ch>
2019-12-14 17:52:00 +0100

    Vorticity now displays streamlines

--- src/winds_vorticity.cpp
+++ src/winds_vorticity.cpp
@@ -92,33 +92,33 @@ public:
         int state = buttonWidget->GetSliderRepresentation()->GetState();
         if (state == 0) {
             // disable time _30
-            renderer->RemoveActor(glyphActors[2]);
+            renderer->RemoveActor(sgridActors[2]);
             renderer->RemoveActor2D(texts[2]);
 
             // enable time _10
-            renderer->AddActor(glyphActors[0]);
+            renderer->AddActor(sgridActors[0]);
             renderer->AddActor2D(texts[0]);
         } else if (state == 1) {
             // disable time _10
-            renderer->RemoveActor(glyphActors[0]);
+            renderer->RemoveActor(sgridActors[0]);
             renderer->RemoveActor2D(texts[0]);
 
             // enable time _20
-            renderer->AddActor(glyphActors[1]);
+            renderer->AddActor(sgridActors[1]);
             renderer->AddActor2D(texts[1]);
         } else {
             // disable time _20
-            renderer->RemoveActor(glyphActors[1]);
+            renderer->RemoveActor(sgridActors[1]);
             renderer->RemoveActor2D(texts[1]);
 
             // enable time _30
-            renderer->AddActor(glyphActors[2]);
+            renderer->AddActor(sgridActors[2]);
             renderer->AddActor2D(texts[2]);
         }
 
     }
-    vtkButtonCallback() : glyphActors(std::vector<vtkSmartPointer<vtkActor>>(0)), renderer(0), actor(0), renderWindow(0), renderWindowInteractor(0), styleTrackball(0), texts(std::vector<vtkSmartPointer<vtkTextActor>>(0)){}
-    std::vector<vtkSmartPointer<vtkActor>> glyphActors;
+    vtkButtonCallback() : sgridActors(std::vector<vtkSmartPointer<vtkActor>>(0)), renderer(0), actor(0), renderWindow(0), renderWindowInteractor(0), styleTrackball(0), texts(std::vector<vtkSmartPointer<vtkTextActor>>(0)){}
+    std::vector<vtkSmartPointer<vtkActor>> sgridActors;
     vtkSmartPointer<vtkRenderer> renderer;
     vtkSmartPointer<vtkActor> actor;
     vtkSmartPointer<vtkRenderWindow> renderWindow;
@@ -180,7 +180,7 @@ int main(int, char *[]) {
 
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
     renderWindowInteractor->SetRenderWindow(renderWindow);
-    renderer->SetBackground(1, 1, 1);
+    renderer->SetBackground(0, 0, 0);
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Creating the button to select what time slot to display
@@ -257,69 +257,26 @@ int main(int, char *[]) {
         vector_fields[i] = vector_field;
     }
     
-    
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// Converting cells to structured grids
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    cerr << "Converting cells to structured grids...";
-    time = clock();
-    std::vector<vtkSmartPointer<vtkCellDataToPointData>> cellsToGrids(3);
-    std::vector<vtkSmartPointer<vtkStructuredGrid>> sgrids(3);
-    for (int i = 0 ; i < 3 ; i++) {
-        vtkSmartPointer<vtkCellDataToPointData> cellToGrid = vtkSmartPointer<vtkCellDataToPointData>::New();
-        cellToGrid->SetInputData(vtkStructuredGrid::SafeDownCast(vector_fields[i]));
-        cellToGrid->PassCellDataOn();
-        cellToGrid->Update();
-        
-        cellsToGrids[i] = cellToGrid;
-        
-        // Creating the structured grid from the data
-        vtkSmartPointer<vtkStructuredGrid> sgrid = vtkSmartPointer<vtkStructuredGrid>::New();
-        sgrid = cellToGrid->GetStructuredGridOutput();
-        cerr << "lol" << endl;
-        
-        sgrids[i] = sgrid;
-        sgrid->Print(cerr);
-    }
-    cerr << " done (" << seconds(time) << " s)" << endl;
-    
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// Creating the vectors
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    cerr << "Converting cells to structured grids...";
-    time = clock();
-    std::vector<vtkSmartPointer<vtkFloatArray>> vectors(3);
-    for (int i = 0 ; i < 3 ; i++) {
-        vtkSmartPointer<vtkFloatArray> vector = vtkSmartPointer<vtkFloatArray>::New();
-        vector->SetNumberOfComponents(3);
-        for (int j = 0 ; j < 149 ; j++) {
-            double v[3];
-            v[0] = sgrids[i]->GetPointData()->GetArray("ua")->GetComponent(j,0);
-            v[1] = sgrids[i]->GetPointData()->GetArray("va")->GetComponent(j,0);
-            v[2] = sgrids[i]->GetPointData()->GetArray("wa")->GetComponent(j,0);
-            
-            vector->InsertTuple(j, v);
-        }
-        
-        sgrids[i]->GetPointData()->SetVectors(vector);
-        
-        vectors[i] = vector;
-    }
-    cerr << " done (" << seconds(time) << " s)" << endl;
-    
-    
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// Creating the structured grids and hedgehogs
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     cerr << "Setting up the hedgehogs...";
     time = clock();
+    std::vector<vtkSmartPointer<vtkStructuredGrid>> sgrids(3);
     std::vector<vtkSmartPointer<vtkHedgeHog>> hedgehogs(3);
     std::vector<vtkSmartPointer<vtkPolyDataMapper>> sgridMappers(3);
     std::vector<vtkSmartPointer<vtkActor>> sgridActors(3);
     for (int i = 0 ; i < 3 ; i++) {
+        // Creating the structured grid from the data
+        vtkSmartPointer<vtkStructuredGrid> sgrid = vtkSmartPointer<vtkStructuredGrid>::New();
+        sgrid = vtkStructuredGrid::SafeDownCast(vector_fields[i]);
+        
         // Creating the hedgehog
         vtkSmartPointer<vtkHedgeHog> hedgehog = vtkSmartPointer<vtkHedgeHog>::New();
-        hedgehog->SetInputData(sgrids[i]);
+        hedgehog->SetInputData(sgrid);
+        hedgehog->SetInputConnection(readers[i]->GetOutputPort());
+        hedgehog->GetInputPortInformation(0);
+        hedgehog->Update();
         
         // Creating the sgrid mapper
         vtkSmartPointer<vtkPolyDataMapper> sgridMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
@@ -328,8 +285,8 @@ int main(int, char *[]) {
         // Creating the sgrid actor
         vtkSmartPointer<vtkActor> sgridActor = vtkSmartPointer<vtkActor>::New();
         sgridActor->SetMapper(sgridMapper);
-        sgridActor->GetProperty()->SetColor(0,0,0);
         
+        sgrids[i] = sgrid;
         hedgehogs[i] = hedgehog;
         sgridMappers[i] = sgridMapper;
         sgridActors[i] = sgridActor;
@@ -386,7 +343,7 @@ int main(int, char *[]) {
     /// Enabling the time button
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     vtkSmartPointer<vtkButtonCallback> callbackButton = vtkSmartPointer<vtkButtonCallback>::New();
-//    callbackButton->glyphActors = glyphActors;
+    callbackButton->sgridActors = sgridActors;
     callbackButton->renderer = renderer;
     callbackButton->actor = renderer->GetActors()->GetLastActor();
     callbackButton->renderWindow = renderWindow;
